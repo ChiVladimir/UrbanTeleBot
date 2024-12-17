46a7b21137b39e95917ed3791ccc5b20ad21d556
Владимир <chivladimir@yandex.ru>
2024-12-17 17:48:50 +0300

    UrbanTeleBot

--- .venv/main.py
+++ .venv/main.py
@@ -3,7 +3,7 @@ from aiogram import Bot, Dispatcher, types, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
-api = "8110042185:AAERDVOgOu7wPQieCCHMX-oCgONG7T438Oc"
+api = ""
 bot = Bot(token=api)
 dp  = Dispatcher(bot, storage= MemoryStorage())
 

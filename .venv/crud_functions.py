b34278547b499ded0bdd50b277635818a50f1393
Vladimir Chichetkin <chivladimir@yandex.ru>
2024-12-27 12:30:00 +0300

    UrbanTeleBot. Module 14 hw 5

--- .venv/crud_functions.py
+++ .venv/crud_functions.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-connection = sqlite3.connect("database.db", timeout=10)
+connection = sqlite3.connect("database_health.db", timeout=10)
 cursor = connection.cursor()
 
 def initiate_db():
@@ -10,6 +10,16 @@ def initiate_db():
     title TEXT NOT NULL,
     description TEXT,
     price INTEGER
+    )
+    ''')
+
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Users(
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER,
+    balance INTEGER NOT NULL
     )
     ''')
     connection.commit()
@@ -30,10 +40,21 @@ def get_all_products():
     get_prod = cursor.fetchall()
     return get_prod
 
+def add_user(username, email, age):
+    cursor.execute("INSERT INTO Users (username, email, age, balance ) VALUES (?, ?, ?, ?)", (username, email, age, 1000))
+    connection.commit()
+
+def is_included(username):
+    check_user = cursor.execute("SELECT * FROM Users WHERE username = ?", (username,))
+    if check_user.fetchone() is not None:
+        return True
+    connection.commit()
+    return False
+
 if __name__ == '__main__':
-#    initiate_db()
-#    filling_data()
+    initiate_db()
+    filling_data()
 #    killing_data()
-#    print(get_all_products())
+    print(get_all_products())
     connection.close()
 

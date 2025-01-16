9adc47375b6cde926e7695a5fbd31a174909d04c
Vladimir Chichetkin <chivladimir@yandex.ru>
2025-01-16 10:43:05 +0300

    UrbanTeleBot. Module 14 hw 5

---
+++ .venv/UrbanBot/config.py
@@ -0,0 +1,7 @@
+API = "8110042185:AAERDVOgOu7wPQieCCHMX-oCgONG7T438Oc"
+
+manikur_cost = 1700
+pedikur_cost = 1200
+narast_cost = 3100
+
+admins = [410011143]
---
+++ .venv/UrbanBot/old/qq.py
@@ -0,0 +1,12 @@
+def sum(data):
+    sum = 0
+    for i in data:
+        sum += 1
+    return sum
+data = [1,2,3]
+print(sum(
+    data
+))
+
+for i in range(2,8,2):
+    print(i*3)
